b8c5f64cf5fd51aac1e72233906d96e2473e74bc
DennisMat <dennis.toronto.email@gmail.com>
2020-04-11 17:44:39 -0500

    More changes

--- applications/spring-boot-example/src/main/java/hello/Application.java
+++ applications/spring-boot-example/src/main/java/hello/Application.java
@@ -17,23 +17,38 @@ public class Application {
 	@RequestMapping("/")
 	public String home() {
 		try {
-			
-			
-			final String uri = "dennisservice2:8094/insideapp";
-		     
-		    RestTemplate restTemplate = new RestTemplate();
-		    String resultInsideApp = "Inside app not working";
-		    try {
-		    	resultInsideApp = restTemplate.getForObject(uri, String.class);
-		    } catch (Exception e) {
-				
-			}
 
-			return "Hello from outside app, from dennis " 
-		    + "<br>Ip adress = " + InetAddress.getLocalHost().getHostAddress()
-		    + "<br>Time = " + (new Date()).toString()
-		    + "<br><br><br>"+resultInsideApp;
-		    
+			final String url1 = "http://dennisservice2:8094/insideapp";
+			final String url2 ="http://dennisservice2.default.svc.cluster.local:8094/insideapp";
+			final String url3 = "http://dennisservice2:8090/insideapp";
+			final String url4="http://dennisservice2.default.svc.cluster.local:8090/insideapp";
+			RestTemplate restTemplate = new RestTemplate();
+			String resultInsideApp = "Inside app not working";
+			try {
+				resultInsideApp = restTemplate.getForObject(url1, String.class);
+				resultInsideApp+="url1 works";
+			} catch (Exception e) {}
+
+			try {
+				resultInsideApp += restTemplate.getForObject(url2, String.class);
+				resultInsideApp+="url2 works";
+			} catch (Exception e) {}
+			
+			try {
+				resultInsideApp += restTemplate.getForObject(url3, String.class);
+				resultInsideApp+="url3 works";
+			} catch (Exception e) {}
+			
+			try {
+				resultInsideApp += restTemplate.getForObject(url4, String.class);
+				resultInsideApp+="url4 works";
+			} catch (Exception e) {}
+
+			return "Hello from outside app v7 , from dennis " 
+			+ "<br>Ip adress = " + InetAddress.getLocalHost().getHostAddress()
+			+ "<br>Time = " + (new Date()).toString()
+			+ "<br><br><br>"+resultInsideApp;
+
 		} catch (Exception e) {
 			return "";
 		}
